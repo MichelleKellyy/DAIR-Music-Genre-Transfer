0a9c3ccfee22b54e6ee9e18e86b6e838b94c8907
MichelleKellyy <michellebrookskelly@gmail.com>
2025-01-15 12:43:07 -0500

    Update adversarial_classifier.py

--- model/adversarial_classifier.py
+++ model/adversarial_classifier.py
@@ -1,3 +1,19 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
+
+class AdversarialClassifier(nn.Module):
+    def __init__(self, num_genres, latent_dim, hidden_dim=128):
+        super(AdversarialClassifier, self).__init__()
+        self.fc1 = nn.Linear(latent_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(latent_dim, num_genres)
+        self.dropout = nn.Dropout(0.3)
+
+def forward(self,z):
+    x = F.relu(self.fc1(z))
+    x = self.dropout(x)
+    x = F.relu(self.fc(x))
+    x = self.dropout(x)
+    x = self.fc3(x)
+    return x
