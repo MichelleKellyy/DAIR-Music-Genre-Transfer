de5dd50470adbb1684e9411e61d5e6f157e02826
usern4 <ciciw8888@gmail.com>
2025-02-05 20:58:29 -0500

    Decided on tokenizer config

--- data_analysis/REMI_Tokenization.py
+++ data_analysis/REMI_Tokenization.py
@@ -2,34 +2,33 @@ from miditok import REMI, TokenizerConfig
 from pathlib import Path
 
 # Tokenizer parameters
-# TODO: Decide whether we want velocities as False
 TOKENIZER_PARAMS = {
-    "pitch_range": (21, 109),
+    "pitch_range": (21, 109), # Range of note pitches to use
     "beat_res": {(0, 4): 8, (4, 12): 4},  # Samples per beat
-    "num_velocities": 32,  # Number of velocity bins
     "special_tokens": ["PAD", "BOS", "EOS", "MASK"],
+    "use_velocities": False,
     "use_chords": True,
-    "use_rests": False,
+    "use_rests": True,
     "use_tempos": True,
     "use_time_signatures": False,
     "use_programs": True,
+    "use_pitch_drum_tokens": True,
     "num_tempos": 32,  # Number of tempo bins
     "tempo_range": (40, 250),  # (Min, Max)
+    "chord_unknown": None, # Set unknown chords to None
+    "programs": list(range(-1, 128)),  # Sequence of MIDI programs to use
+    "one_token_stream_for_programs": False,
+    "program_changes": False,  # Place Program token for each note
 }
 config = TokenizerConfig(**TOKENIZER_PARAMS)
 
 # Creates the tokenizer
 tokenizer = REMI(config)
+midi_paths = list(Path("./dataset").glob('*.mid'))
 
-# TODO: Split MIDI files into one track per instrument
-# TODO: Figure out vocabulary size for the training
-# TODO: Train the tokenizer (BPE)
+# Train the tokenizer (BPE)
+tokenizer.train(vocab_size=30000, model="BPE", files_paths=midi_paths)
+tokenizer.save("tokenizer.json")
 
 # Tokenize a MIDI file
-tokens = tokenizer(Path("./dataset/000aec55332d26c818c0d6cf6af40010.mid"))  # Testing one midi file for now
-
-# Convert to MIDI and save it
-generated_midi = tokenizer(tokens)
-generated_midi.dump_midi(Path("/tokenized_dataset/test_midi_tokenization.mid"))
-
-# TODO: Tokenize the whole dataset and store it in JSON file as well as the tokenized MIDI files
+tokens = tokenizer(midi_paths)  # Testing one midi file for now
